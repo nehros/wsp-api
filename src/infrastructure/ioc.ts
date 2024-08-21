89a1af7fee2bd6a4133702c137168c2c1066c77f
nehros <contacto.nehros@gmail.com>
2024-08-20 21:15:57 -0300

    Add files via upload

---
+++ src/infrastructure/ioc.ts
@@ -0,0 +1,29 @@
+import { ContainerBuilder } from "node-dependency-injection";
+import { LeadCreate } from "../application/lead.create";
+import LeadCtrl from "./controller/lead.ctrl";
+import MetaRepository from "./repositories/meta.repository";
+import MockRepository from "./repositories/mock.repository";
+import TwilioService from "./repositories/twilio.repository";
+import WsTransporter from "./repositories/ws.external";
+import { VenomTransporter } from "./repositories/venom.repository";
+
+const container = new ContainerBuilder();
+
+/**
+ * Inicamos servicio de WS / Bot / Twilio
+ */
+container.register("ws.transporter", VenomTransporter);
+const wsTransporter = container.get("ws.transporter");
+
+container.register("db.repository", MockRepository);
+const dbRepository = container.get("db.repository");
+
+container
+  .register("lead.creator", LeadCreate)
+  .addArgument([dbRepository, wsTransporter]);
+
+const leadCreator = container.get("lead.creator");
+
+container.register("lead.ctrl", LeadCtrl).addArgument(leadCreator);
+
+export default container;
